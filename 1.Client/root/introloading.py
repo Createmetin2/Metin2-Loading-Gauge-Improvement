6bbad83d93e74c4eb229c49c2ca488e2910c69b3
Muhammet <muhammetuzun5@gmail.com>
2020-09-12 18:30:50 +0300

    Update introloading.py

--- 1.Client/root/introloading.py
+++ 1.Client/root/introloading.py
@@ -1,14 +1,17 @@
 #Find
-			self.loadingGage.SetPercentage(2+98*p/100, 100)
-			
-#Change
-			self.loadingGage.SetPercentage(p, 160)
-		
+import playerSettingModule
+
+#Add
+IsLoaded = False
+	
 #Find
 		self.__SetProgress(0)
 		
 #Add Above		
-		tmpLoadStepList = tuple(zip(*self.loadStepList))[0]
-		for progress in set(range(tmpLoadStepList[0], tmpLoadStepList[-1] + 1)).difference(tmpLoadStepList):
-			self.loadStepList.append((progress, lambda: None))
-		self.loadStepList.sort()
+		global IsLoaded
+		if not IsLoaded:
+			tmpLoadStepList = tuple(zip(*self.loadStepList))[0]
+			for progress in set(range(tmpLoadStepList[0], tmpLoadStepList[-1] + 1)).difference(tmpLoadStepList):
+				self.loadStepList.append((progress, lambda: None))
+			self.loadStepList.sort()
+			IsLoaded = True
